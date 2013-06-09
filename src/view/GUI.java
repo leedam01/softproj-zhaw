78ebcdb803b88cd45816dfa26a1dba2e2aba6793
Olivier <oli.fa@bluewin.chgit config --global user.name PinkSheepgit config --global user.email oli.fa@bluewin.ch>
2013-06-09 14:49:58 +0200

    adressbook added

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,41 +1,47 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
 
 import sender.Message;
 
 import com.toedter.calendar.JDateChooser;
 
 public class GUI extends JFrame {
-	private JPanel p2, p3;
+	private JPanel p2, p3, p4, p5;
 	private JRadioButton radioEmail, radioSms, radioMms, radioPrinter;
-	private JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11;
-	private JTextField text1, text2, text3, text4, text5, text6, text7, text8;
+	private JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13;
+	private JTextField text1, text2, text3, text4, text5, text6, text7, text8, text9;
 	private JTextArea area1;
-	private JButton b1, b2, b3, b4, b5, b6;
+	private JButton b1, b2, b3, b4, b5, b6, b7, b8;
 	private GroupLayout layout;
 	private JCheckBox checkRemainder;
-
+	private JList<String> adressList;
 	private JDateChooser date;
+	private DefaultListModel<String> limo;
+	private JDialog dialog;
 
 	public GUI(Message m) {
 
@@ -58,12 +64,6 @@ public class GUI extends JFrame {
 		// Turn on automatically creating gaps between components that touch
 		// the edge of the container and the container.
 		layout.setAutoCreateContainerGaps(true);
-
-		/*
-		 * // Create a sequential group for the horizontal axis. text1 = new
-		 * JTextField(20); text2 = new JTextField(20); l1 = new
-		 * JLabel("Betreff"); l2 = new JLabel("Absender");
-		 */
 
 		p2 = new JPanel();
 		l1 = new JLabel("Messagetype:");
@@ -94,13 +94,16 @@ public class GUI extends JFrame {
 
 		createAllFields();
 		setEmailForm();
+		
 
-		// layout.setHorizontalGroup(hGroup);
-		// layout.setVerticalGroup(vGroup);
+		b8 = new JButton("Auswählen");
+		b7 = new JButton("Hinzufügen");
+
 
 		add(panel);
 		setVisible(true);
-		setSize(600, 400);
+		pack();
+		//setSize(600, 400);
 	}
 
 	public void createAllFields() {
@@ -147,15 +150,17 @@ public class GUI extends JFrame {
 								.addComponent(l2).addComponent(l3)
 								.addComponent(l4).addComponent(l5)
 								.addComponent(l9).addComponent(l6)
-								.addComponent(l7).addComponent(l8).addComponent(l10).addComponent(l11))
+								.addComponent(l7).addComponent(l8)
+								.addComponent(l10).addComponent(l11))
 				.addGroup(
 						layout.createParallelGroup(GroupLayout.Alignment.CENTER)
 								.addComponent(text1).addComponent(text2)
 								.addComponent(text3).addComponent(text4)
 								.addComponent(text7).addComponent(area1)
 								.addComponent(text5).addComponent(text6)
-								.addComponent(checkRemainder).addComponent(text8)
-								.addComponent(date).addComponent(p3))
+								.addComponent(checkRemainder)
+								.addComponent(text8).addComponent(date)
+								.addComponent(p3))
 				.addGroup(
 						layout.createParallelGroup(GroupLayout.Alignment.CENTER)
 								.addComponent(b1).addComponent(b2)
@@ -201,16 +206,16 @@ public class GUI extends JFrame {
 								.addComponent(b4))
 				.addGroup(
 						layout.createParallelGroup(
-								GroupLayout.Alignment.BASELINE)
-								.addComponent(checkRemainder))
+								GroupLayout.Alignment.BASELINE).addComponent(
+								checkRemainder))
 				.addGroup(
 						layout.createParallelGroup(
 								GroupLayout.Alignment.BASELINE)
-								.addComponent(l10).addComponent(text8))				
+								.addComponent(l10).addComponent(text8))
 				.addGroup(
 						layout.createParallelGroup(
 								GroupLayout.Alignment.BASELINE)
-								.addComponent(l11).addComponent(date))				
+								.addComponent(l11).addComponent(date))
 				.addGroup(
 						layout.createParallelGroup(
 								GroupLayout.Alignment.BASELINE)
@@ -295,7 +300,7 @@ public class GUI extends JFrame {
 		b4.setVisible(false);
 
 		checkRemainder.setVisible(true);
-		
+
 		if (checkRemainder.isSelected()) {
 			l10.setVisible(true);
 			text8.setVisible(true);
@@ -308,7 +313,7 @@ public class GUI extends JFrame {
 
 			l11.setVisible(false);
 			date.setVisible(false);
-		}		
+		}
 		p2.setVisible(true);
 	}
 
@@ -342,7 +347,7 @@ public class GUI extends JFrame {
 		b4.setVisible(true);
 
 		checkRemainder.setVisible(true);
-		
+
 		if (checkRemainder.isSelected()) {
 			l10.setVisible(true);
 			text8.setVisible(true);
@@ -389,23 +394,73 @@ public class GUI extends JFrame {
 		b4.setVisible(false);
 
 		checkRemainder.setVisible(false);
-		
+
 		l10.setVisible(false);
 		text8.setVisible(false);
-		
+
 		l11.setVisible(false);
 		date.setVisible(false);
 
 		p2.setVisible(true);
 	}
-	
-	public void setRemainderFields(boolean b){
-		
+
+	public void setRemainderFields(boolean b) {
+
 		l10.setVisible(b);
 		text8.setVisible(b);
-		
+
 		l11.setVisible(b);
 		date.setVisible(b);
+	}
+
+	public void createDialog(ArrayList<String> li) {
+
+		dialog = new JDialog();
+
+		dialog.setTitle("Adressbuch");
+		
+		l12 = new JLabel("Neuer Kontakt hinzufügen:");
+		text9 = new JTextField(20);
+		
+		limo = new DefaultListModel<String>();
+		
+		for (String field:li){
+			limo.addElement(field);
+		}
+		
+		adressList = new JList<String>(limo);
+		l13 = new JLabel("Kontakt auswählen");
+		
+        adressList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        adressList.setVisibleRowCount(5);
+        adressList.setPreferredSize(new Dimension(100,100));
+        JScrollPane listScrollPane = new JScrollPane(adressList);
+		
+        dialog.setLayout(new BorderLayout());
+		
+		adressList.setPreferredSize(new Dimension(200,100));
+		
+		
+		p4 = new JPanel();
+		p5 = new JPanel();
+		p4.setLayout(new FlowLayout());
+		p5.setLayout(new FlowLayout());
+		
+		p4.add(l12);
+		p4.add(text9);
+		p4.add(b7);
+		
+		p5.add(l13);
+		p5.add(listScrollPane);
+		p5.add(b8);
+		
+		dialog.add(p4,BorderLayout.NORTH);
+		dialog.add(p5,BorderLayout.CENTER);
+		
+		dialog.pack();
+		dialog.setModal(true);
+		dialog.setVisible(true);
+		
 	}
 
 	public void setRadioListener(ActionListener a) {
@@ -418,9 +473,16 @@ public class GUI extends JFrame {
 	public void setSendListener(ActionListener a) {
 		this.b2.addActionListener(a);
 	}
-	
-	public void setCheckboxListener(ItemListener i){
+
+	public void setCheckboxListener(ItemListener i) {
 		this.checkRemainder.addItemListener(i);
+	}
+
+	public void setAdressbookListener(ActionListener a) {
+		this.b1.addActionListener(a);
+		this.b2.addActionListener(a);
+		this.b7.addActionListener(a);
+		this.b8.addActionListener(a);
 	}
 
 	public JRadioButton getRadioEmail() {
@@ -438,4 +500,46 @@ public class GUI extends JFrame {
 	public JRadioButton getRadioPrinter() {
 		return radioPrinter;
 	}
+
+	public JButton getB7() {
+		return b7;
+	}
+
+	public JButton getB8() {
+		return b8;
+	}
+
+	public DefaultListModel<String> getLimo() {
+		return limo;
+	}
+
+	public JTextField getText9() {
+		return text9;
+	}
+
+	public JList<String> getAdressList() {
+		return adressList;
+	}
+
+	public JButton getB1() {
+		return b1;
+	}
+
+	public JButton getB2() {
+		return b2;
+	}
+
+	public JDialog getDialog() {
+		return dialog;
+	}
+
+	public JTextField getText1() {
+		return text1;
+	}
+
+	public JTextField getText2() {
+		return text2;
+	}
+	
+	
 }
