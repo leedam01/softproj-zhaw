f2d0980419411dc8e37ebafa92b5e421543fc6b1
kressi <mkressibucher@hotmail.com>
2013-06-09 20:41:21 +0200

    iterator fixed

--- src/ch/zhaw/multichannel/messenger/Messenger.java
+++ src/ch/zhaw/multichannel/messenger/Messenger.java
@@ -1,8 +1,11 @@
 package ch.zhaw.multichannel.messenger;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 import ch.zhaw.multichannel.message.Message;
 import ch.zhaw.multichannel.message.MessageType;
@@ -13,11 +16,24 @@ public class Messenger implements MessageType, Runnable {
 	private List<Message> messageArchive;
 	
 	public Messenger() {
-
+		this.messages = new ArrayList<Message>();
+		this.messageArchive = new ArrayList<Message>();
 	}
 	
 	public List<Message> getMessages() {
 		return messages;
+	}
+	
+	public Message getMessageById(UUID msgId) {
+		Message msg;
+		Iterator <Message> msgIter = messages.iterator();
+		while (msgIter.hasNext()) {
+			msg = msgIter.next();
+			if (msgId.equals(msg.getMsgId())) {
+				return msg;
+			}
+		}
+		return null;
 	}
 	
 	public void addMessage(Message msg) {
@@ -64,19 +80,19 @@ public class Messenger implements MessageType, Runnable {
 
 		Message msg;
 		Date currentTime = new Date();
+		Iterator <Message> msgIter;
 		
 		while (true) {
 			synchronized (messages) {
-				if (messages.size() > 0) {
-					if (messages.listIterator().hasNext()) {
-						msg = messages.listIterator().next();
-						currentTime.setTime(System.currentTimeMillis());
-						if (msg.getTimeToNotify().compareTo(currentTime) >= 0) {
-							this.sendNotification(msg);
-						}
-						if (msg.getTimeToSend().compareTo(currentTime) >= 0) {
-							this.sendMessage(msg);
-						}
+				msgIter = messages.iterator();
+				while (msgIter.hasNext()) {
+					msg = messages.listIterator().next();
+					currentTime.setTime(System.currentTimeMillis());
+					if (msg.getTimeToNotify().compareTo(currentTime) >= 0) {
+						this.sendNotification(msg);
+					}
+					if (msg.getTimeToSend().compareTo(currentTime) >= 0) {
+						this.sendMessage(msg);
 					}
 				}
 				try {
